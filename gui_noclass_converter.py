eea9042ea730f0e244b9def345ebbf7632ad2690
Orçun Madran <orcunmadran@gmail.com>
2018-04-11 00:52:46 +0300

    Update gui_noclass_converter.py

--- gui_noclass_converter.py
+++ gui_noclass_converter.py
@@ -28,6 +28,8 @@ ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
 ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
 
 ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
+#Yazı tipi kontrolü
+#ttk.Label(mainframe, text="feet", font=("Arial", 20)).grid(column=3, row=1, sticky=W)
 ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
 ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
 
