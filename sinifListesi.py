40c9420d649a94254e1d2747c4024ef33a955597
Orçun Madran <orcunmadran@gmail.com>
2019-04-24 16:13:12 +0300

    Sınıf Listesi Çalışması
    Sınıf listesinden gelişigüzel öğrenci seçen uygulama

---
+++ sinifListesi.py
@@ -0,0 +1,45 @@
+class Rastgele:
+    def ogrenciSec(self):
+        from random import choice
+        liste = ["MEHMET GÜNÇE",\
+        "KAAN BEHA ÖKER",\
+        "FATMA TAŞ",\
+        "ALEYNA YILMAZ",\
+        "HİBE DEĞER",\
+        "ESLEM AYDEMİR",\
+        "ARDIL SERHAT BİLGİÇ",\
+        "CANAN BOZKURT",\
+        "MİHRİBAN CİVAROĞLU",\
+        "SERENGÜL GÖZAÇIK",\
+        "UĞUR KARAAĞAÇ",\
+        "GAMZE KÖKSAL",\
+        "CANSEL KUNDUKAN",\
+        "METE ÖZCAN",\
+        "ONUR ÖZTÜRK",\
+        "YASEMİN ÖZTÜRK",\
+        "ALPER TEMİZ",\
+        "BUKET ÜREN",\
+        "HASAN ONUR BELOVACIKLI",\
+        "VOLKAN AKDENİZ",\
+        "ESRA BEKAR",\
+        "NİHAT FURKAN ÇİFTÇİ",\
+        "BORAN DEMİR",\
+        "HARUN SEFA DERE",\
+        "BESTE DOĞANAY",\
+        "TANKUT YAĞIZ EROL",\
+        "BAHRİ CAN GÜNGÖR",\
+        "SEVDE KILIÇER",\
+        "GÖKHAN KOZAN",\
+        "ALPER ÖLMEZ",\
+        "KAAN ÖZTÜRK",\
+        "MUSTAFA PAYDAŞ",\
+        "CEREN BİLGE SEFEROĞLU",\
+        "SERDAR SEVİN",\
+        "NURETTİN ŞEN",\
+        "DİLARA TURHAN",\
+        "AYŞE YELİZ TÜRK",\
+        "CEMRE YAZAR",\
+        "BUSE YILDIRIM",\
+        "MÜBERRA YILMAZ",\
+        "SABINA AMRAHOVA"]
+        return (choice(liste))
---
+++ sinifListesiYazdir.py
@@ -0,0 +1,4 @@
+from sinifListesi import Rastgele
+secimYap = Rastgele()
+for i in range(5):
+    print(str(i+1)+" "+secimYap.ogrenciSec())
